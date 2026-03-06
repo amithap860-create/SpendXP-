e91dee95060cb920d0feaf9bdfbf010d66661c31
Amitha Prakash <amithap860@gmail.com>
2026-03-06 19:16:12 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7609169345-afafa",
+  "appId": "1:1075590915627:web:9a890c67348fa01207ac6e",
+  "apiKey": "AIzaSyDcG6-8EVL4RKw0Tk08WyYjy-L-7z4zg2c",
+  "authDomain": "studio-7609169345-afafa.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1075590915627"
+};
